0972d7a2af8815cd47df74b14b579ec65014aeb2
BraianChebib <lucianochebib@gmail.com>
2023-03-26 20:06:41 -0300

    incorporacion de animacion de conteo en el home y redireccionamiento en otra pestaña de los link de las redes sociales

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import ImageSlider from "../../Components/carousel/ImageSlide";
 import { SlideData } from "../../Components/carousel/SlideData";
 import Chatbot from "../../Components/chatbot/chatbot";
+import CountAnimation from "../../Components/CountAnimation/CountAnimation";
 
 export default function Home() {
   return (
     <>
       <ImageSlider slides={SlideData} />
       <Chatbot />
+      <CountAnimation />
     </>
   );
 }
